c6a4a462f76dedc447af951741e82a7a6e8c0e69
Fosuna-Code <ferosuna2001@gmail.com>
2022-11-19 10:49:42 -0700

    put a resizeEvent to change the behavior of the toggle button relate to the screen size and i made some changes in the styles to fit the result section and avoid bugs'cause by insert too many numbers

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+
+
 const body = document.querySelector('body');
 //this is the result section
 
+const result = document.querySelector('.result');
 const operation = document.querySelector('.operation');
 const showResult = document.querySelector('.showResult');
+// const arrowSvg = document.querySelector('.arrow');
 
 const all_clean = document.querySelector('.all_clean');
 const single_clean = document.querySelector('.single_clean');
@@ -14,8 +18,29 @@ const numbers =  document.querySelectorAll('.number');
 const equal = document.querySelector('.showResultOperator')
 
 const themeToggle = document.querySelector('.themeToggle');
+const opacityDefault = '1';
+const opacityHide = 0.1;
 
-//counting numbers and operations (if the)
+themeToggle.style.opacity = opacityDefault;
+// arrowSvg.style.display = 'none';
+
+//fitty module
+fitty('#finalResult', {
+    maxSize: 48,
+});
+fitty('#operationId', {
+    minSize: 20,
+    maxSize: 30,
+    multiLine: true,
+
+});
+
+//getting an array with all the operators (operatorsValue)
+let operatorsToArray = Array.from(operators);
+let operatorsValue = operatorsToArray.map(operator => {return operator.innerText});
+    // console.log(operatorsValue)
+let countingNumbers;
+
 
 themeToggle.addEventListener('click',() =>{
     body.classList.toggle('ligthTheme')
@@ -30,43 +55,83 @@ themeToggle.addEventListener('click',() =>{
     });
 });
 
+//toogle behavor in queries
+function screenHandler(){
+    const windowScreen = window.matchMedia('(max-width: 575px)');
+    if (windowScreen.matches){
+        if ( operation.innerText != '0'){
+            themeToggle.style.opacity = opacityHide;
+        }else{
+            themeToggle.style.opacity = opacityDefault;
+        }
+    }else{
+        themeToggle.style.opacity = opacityDefault;
+    }
+}
+window.onresize = screenHandler;
+
+
 //setting the first div operation
 operation.innerHTML +='<div style="display: inline-block"> <span>0</span> </div>'
 
 function avoidEmptyStr(){
     operation.innerHTML = '<span>0</span>';// "‎"; // invisible character
+
 }
+
+function sharedArrayValue(arr1,arr2){
+    return arr1.some(e => arr2.includes(e));
+}
+// function arrowSvgUp(scrollingElement){
+//     scrollingElement.scrollBy({
+//         top: 1000,
+//         behavior: 'smooth'
+//       });
+// }
+
+
+// arrowSvg.addEventListener('click',() =>{
+//     console.log('hola');
+//     result.scrollBy({
+//         top: -1000,
+//         behavior: 'smooth'
+//       });
+// })
 
 //Events 
 
 single_clean.addEventListener('click', () => {
     // operation.innerText = operation.innerText.slice(0,-1);
     
-    if (operation.lastChild.lastChild){
+    if (operation.lastChild){
         operation.lastChild.removeChild(operation.lastChild.lastChild);
 
     }
     if (operation.innerText.length === 0){
         avoidEmptyStr();
     }
+
+    if(operation.innerText.length == 1 && operation.innerText == 0){
+        themeToggle.style.opacity = opacityDefault;
+    }
 })
 all_clean.addEventListener('click',() => {
     avoidEmptyStr();
     showResult.innerText = '0';
+    themeToggle.style.opacity = opacityDefault;
+
 } )
 
 operators.forEach(element => {
     element.addEventListener('click', ()=>{
-        // if (operation.lastChild.innerText.length >= 21){
-        //     alert('21 digits are the max');
+        // let countingNumbers = operation.lastChild.innerText.includes(operatorsValue) ? 20 : 21;
+        
+        // if (operation.lastChild.innerText.length >= countingNumbers){
+        //     console.log('21 digits are the max');
         //     return;
         // }
 
-        //getting an array with all the operators (operatorsValue)
         let lastCharacterOperation = operation.innerText.charAt(operation.innerText.length - 1);
-        let operatorsToArray = Array.from(operators);
-        let operatorsValue = operatorsToArray.map(operator => {return operator.innerText});
-
         // console.log(operatorsValue);
         if (!operatorsValue.includes(lastCharacterOperation)){
             operation.innerHTML += `<div style="display: inline-block;" ><span class="operatiorOnScreen">${element.innerText}</span></div>`; 
@@ -74,12 +139,24 @@ operators.forEach(element => {
 
     })
 });
+
 numbers.forEach(element => {
     element.addEventListener('click',()=>{
-        // if (operation.lastChild.innerText.length >= 20){
-        //     alert('21 digits are the max');
-        //     return;
-        // }
+        
+        if (themeToggle.style.opacity == opacityDefault && window.matchMedia('(max-width: 575px)').matches){
+            console.log(themeToggle.style.opacity);
+            themeToggle.style.opacity = opacityHide;
+        }
+        
+        // CountingNumbers: you just could put 20 numbers, if there are a
+        // operator in the div, you could put 21 characters, if isn't, you just could put
+        //20
+        countingNumbers = sharedArrayValue(operatorsValue,operation.lastChild.innerText) ? 21 : 20;
+        if (operation.lastChild.innerText.length >= countingNumbers){
+            alert('sorry mate! no more of 20 digits per number');
+            return;
+        }
+
         if ( operation.innerText == 0 ){
             operation.removeChild(operation.lastChild);
             operation.innerHTML +='<div style="display: inline-block">'
@@ -94,11 +171,21 @@ numbers.forEach(element => {
                 operation.lastChild.innerHTML += `<span>${element.innerText}</span>`; 
             }
         }
+        let hasVerticalScrollbar = result.scrollHeight > result.clientHeight;
+        if (hasVerticalScrollbar){
+            result.scrollBy(0, 100);
+            // arrowSvg.style.display = 'block';
+        }
     })
 })
 
 equal.addEventListener('click', () => {
     showResult.innerText = eval(operation.innerText);
+    // let hasVerticalScrollbar = result.scrollHeight > result.clientHeight;
+    //     if (hasVerticalScrollbar){
+    //         result.scrollBy(0, 100);
+    //         arrowSvg.style.top = '50px';
+    //     }
 })
 
 //
